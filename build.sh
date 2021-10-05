ead16c0a200b7cf922f2a39536637b68c7ec6f84
Piotr Kozimor <p1996k@gmail.com>
2021-10-05 20:26:41 +0200

    Switch to OCI format

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ go build .
 cont=$(buildah from gcr.io/distroless/base-debian11)
 buildah copy $cont ip /bin/ip
 buildah config --entrypoint '["/bin/ip"]' $cont
-buildah commit --format docker $cont ip
+buildah commit $cont ip
 echo "👌 Tag nad push latest"
 buildah tag ip docker.io/narciarz96/ip:latest
 buildah push docker.io/narciarz96/ip:latest
